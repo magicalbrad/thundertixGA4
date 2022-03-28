4da10026b51249694ca108100934623dfc221c76
Brad Brown <magicalbrad@gmail.com>
2022-03-28 14:39:35 -0400

    Comments & cleanup

--- parent_frame.js
+++ parent_frame.js
@@ -6,17 +6,24 @@
 ** https://www.simoahava.com/analytics/cookieless-tracking-cross-site-iframes/
 ************************************************************/
 (function() {
-	var childOrigin = 'https://kellieshomesteadrestaurant.thundertix.com';
+	// By default, this will accept messages from any thundertix.com child frame.
+	// Change to your exact domain for increased security.
+	// E.g. childOrigin = 'https://mysubdomain.thundertix.com';
+	var childOrigin = 'thundertix.com$';
 
+	// Controls which iframe events are tracked, and how.
+	// The key (before the colon) is the iframe event to be tracked.
+	// The value (after the colon) is the event to trigger in Google Analytics.
+	// Note: Custom events will require additional setup in analytics to show up in reports.
 	var trackedEvents = {
 		'iframe.gtm.js': 'page_view',
 		'iframe.gtm.linkClick': 'click',
 		'iframe.gtm.scrollDepth': 'scroll',
 		'ticket_purchase': 'ticket_purchase'
-	}
+	};
 
     function postCallback(e) {
-      if (e.origin !== childOrigin) return;
+      if (!e.origin.match(childOrigin)) return;
       if (e.data !== 'childReady' && !e.data.event) return;
 
       if (e.data === 'childReady') {
@@ -35,7 +42,7 @@
 		  // Push dataLayer message from iframe to dataLayer of parent
 		  window.dataLayer.push(event.data);
       }
-    };
+    }
 
     // Start listening for messages from child frame
     window.addEventListener('message', postCallback);
