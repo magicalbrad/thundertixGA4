1fecdf919eb0158a1147b685a44a7d9599cfcd68
Brad Brown <46201035+magicalbrad@users.noreply.github.com>
2022-04-14 20:40:56 -0400

    Update view_item.js

--- view_item.js
+++ view_item.js
@@ -9,7 +9,7 @@
   // Create event using item name scraped from page.
   // As I'm using the embed option that lists all performances, I'm considering the next page
   // where tickets are chosen to be the item page. That URL is "/orders/new" 
-  // This logic *should* also work on the The event page, with URL format of"/events/{{event id}}"
+  // This logic *should* also work on the The event page, with URL format of"/events/(event id)"
   dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
   dataLayer.push({
     'event': 'view_item',
